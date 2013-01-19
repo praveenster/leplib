b6d3a10782be62a7273f052b88703b859098a929
Praveen Moorthy <me@praveenster.com>
2013-01-18 16:24:30 -0800

    First cut abstraction for Sockets.
    Still more functionality to implement.

---
+++ src/Socket.cpp
@@ -0,0 +1,137 @@
+/*
+  Copyright (c) 2011 Neevarp Yhtroom
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+#include "Socket.h"
+
+#ifdef WIN32
+#include "winsock2.h"
+#include "Ws2tcpip.h"
+#define socklen_t int
+inline int SocketGetLastError()
+{
+  return WSAGetLastError();
+}
+
+inline int SocketClose(int socket)
+{
+  shutdown(socket, SD_BOTH);
+  return closesocket(socket);
+}
+#else
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <pthread.h>
+#include <errno.h>
+inline int SocketGetLastError()
+{
+  return errno;
+}
+
+inline int SocketClose(int socket)
+{
+  return close(socket);
+}
+#endif
+
+namespace lepcpplib {
+
+// internal function to convert to struct format used by socket functions.
+// not made as part of class to reduce pollution of native types.
+static void SocketAddressObjectToStruct(SocketAddress& address, sockaddr_in& socket_address)
+{
+  memset(&socket_address, 0, sizeof(socket_address));
+  socket_address.sin_family = AF_INET;
+  socket_address.sin_addr.s_addr = inet_addr((*address.address()).toCharArray());
+  socket_address.sin_port = address.port();
+}
+
+static void SocketAddressStructToObject(sockaddr_in& socket_address, SocketAddress& address)
+{
+  address.set_address(inet_ntoa(socket_address.sin_addr));
+  address.set_port(socket_address.sin_port);
+}
+
+Socket::Socket(AddressFamily address_family, Type type, Protocol protocol)
+  : socket_(0)
+{
+  int f = (address_family == kInternet) ? AF_INET : AF_INET;
+  int t = (type == kStream) ? SOCK_STREAM : SOCK_DGRAM;
+  int p = (protocol == kTcp) ? IPPROTO_TCP : IPPROTO_UDP;
+  socket_ = socket(f, t, p);
+}
+
+Socket::~Socket()
+{
+  this->Close();
+}
+
+void Socket::Close()
+{
+#ifdef WIN32
+  closesocket(socket_);
+#else
+  close(socket_);
+#endif
+}
+
+int Socket::Connect(SocketAddress& destination_address)
+{
+  sockaddr_in socket_address;
+  SocketAddressObjectToStruct(destination_address, socket_address);
+  return connect(socket_, (sockaddr*)&socket_address, sizeof(socket_address));
+}
+
+int Socket::Bind(SocketAddress& server_address)
+{
+  local_address_ = server_address;
+  sockaddr_in socket_address;
+  SocketAddressObjectToStruct(server_address, socket_address);
+  return bind(socket_, (struct sockaddr*) &socket_address, sizeof(socket_address));
+}
+
+int Socket::Listen(int backlog)
+{
+  return listen(socket_, backlog);
+}
+
+SmartPointer<Socket> Socket::Accept(int backlog)
+{
+  sockaddr_in client_socket_address;
+  int address_length = sizeof(client_socket_address);
+  int client_socket = accept(socket_, (struct sockaddr *)&client_socket_address, (socklen_t*)&address_length);
+  SmartPointer<Socket> rs = new Socket(kInternet, kStream, kTcp);
+  SocketAddressStructToObject(client_socket_address, (*rs).remote_address_);
+  (*rs).local_address_ = local_address_;
+  return rs;
+}
+
+int Socket::SetOptionReuse()
+{
+  int value = 1;
+  return setsockopt(socket_, SOL_SOCKET, SO_REUSEADDR, (char*)&value, sizeof(value));
+}
+} // namespace lepcpplib
---
+++ src/Socket.h
@@ -0,0 +1,79 @@
+/*
+  Copyright (c) 2011 Neevarp Yhtroom
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef LEPCPPLIB_SOCKET_H_
+#define LEPCPPLIB_SOCKET_H_
+
+#include "SocketAddress.h"
+
+namespace lepcpplib {
+class Socket {
+  public:
+    enum AddressFamily {
+      kInternet
+    };
+
+    enum Type {
+      kStream,
+      kDatagram
+    };
+
+    enum Protocol {
+      kTcp,
+      kUdp
+    };
+
+    enum Direction {
+      kBoth,
+      kReceive,
+      kSend
+    };
+
+    Socket(AddressFamily address_family, Type type, Protocol protocol);
+    ~Socket();
+
+    void Close();
+    int Connect(SocketAddress& destination_address);
+    int Bind(SocketAddress& server_address);
+    int Listen(int backlog);
+    SmartPointer<Socket> Accept(int backlog);
+
+    int SetOptionReuse();
+
+    void Send();
+    void SendTo();
+    void Shutdown();
+    void Receive();
+    void ReceiveFrom();
+    void GetLocalAddress();
+    void GetRemoteAddress();
+    int GetLastError();
+
+  private:
+    int socket_;
+    SocketAddress local_address_;
+    SocketAddress remote_address_;
+};
+} // namespace lepcpplib
+
+#endif // LEPCPPLIB_SOCKET_H_
